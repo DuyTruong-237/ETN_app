914875a8a38f840df12a57850f9adadc4c590485
DuyTruong-080 <20522080@ms.uit.edu.vn>
2022-09-21 00:02:48 +0700

    ver1
    hoàn thành giao diện đăng nhập

--- etnapp/app/src/main/java/com/example/etnapp/MainActivity.java
+++ etnapp/app/src/main/java/com/example/etnapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.etnapp;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        moveActivity();
+    }
+
+    private void moveActivity() {
+        CountDownTimer countDownTimert=new CountDownTimer(2000,1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+               Intent intent = new Intent(MainActivity.this,LoginActivity.class );
+              startActivity(intent);
+            }
+        };
+        countDownTimert.start();
     }
 }
